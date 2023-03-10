44812e55720b3571aab47e15a251ee9400f3bd35
Nodirbek <mirzajanovnodirbek@gmail.com>
2023-03-10 11:07:46 +0600

    Выводим отог

--- new/Program.cs
+++ new/Program.cs
@@ -1,2 +1,3 @@
 ﻿var people = new string[] { "jgug", "hhhj hhh", "-2" };
 string[] group = Array.FindAll(people, person => person.Length <= 3);
+foreach (var person in group) Console.WriteLine(person);
